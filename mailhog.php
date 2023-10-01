17c7ae045a0d398e0e80ff0a166207d8f339f8f0
Bishwajit Adhikary <bishwajitcadhikary@gmail.com>
2023-10-02 01:39:32 +0600

    Initial Commit

---
+++ mailhog.php
@@ -0,0 +1,176 @@
+<?php
+/*
+Plugin Name: MailPit SMTP for WordPress
+Description: A simple plugin to test SMTP settings and override the default WordPress mail system.
+Version: 1.0
+Author: Bishwajit Adhikary
+Author URI: https://bishwajitadhikary.com
+License: GPLv2 or later
+Text Domain: wp-mailpit-smtp
+*/
+
+// Set default values for SMTP host and port
+function mailpit_smtp_default_options() {
+    add_option('mailpit_smtp_host', '127.0.0.1');
+    add_option('mailpit_smtp_port', '1025');
+}
+
+function mailpit_smtp_settings_menu() {
+    // Define the URL of the custom icon
+    $icon_url = plugin_dir_url(__FILE__) . 'mailpit.svg';
+
+    add_menu_page(
+        'MailPit SMTP Settings',
+        'MailPit SMTP',
+        'manage_options',
+        'mailpit-smtp-settings',
+        'mailpit_smtp_settings_page',
+        $icon_url // Specify the custom icon URL here
+    );
+}
+
+// Display the SMTP settings page
+function mailpit_smtp_settings_page() {
+    ?>
+    <div class="wrap">
+        <h2>MailPit SMTP Settings</h2>
+        <form method="post" action="options.php">
+            <?php
+            settings_fields('mailpit-smtp-settings-group');
+            do_settings_sections('mailpit-smtp-settings');
+            submit_button();
+            ?>
+        </form>
+
+        <h2>Test SMTP Email</h2>
+        <form method="post" action="">
+            <table class="form-table" role="presentation">
+                <tbody>
+                <tr>
+                    <th scope="row">
+                        <label for="subject">Subject:</label>
+                    </th>
+                    <td>
+                        <input type="text" id="subject" name="subject" value="Test Email" required>
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row">
+                        <label for="test_email">Test Email Address:</label>
+                    </th>
+                    <td>
+                        <input type="email" id="test_email" name="test_email" value="<?php echo esc_attr(get_option('admin_email')); ?>" required>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+
+            <p class="submit">
+                <input type="submit" name="send_test_email" class="button-primary" value="Send Test Email">
+            </p>
+        </form>
+        <?php
+        if (isset($_POST['send_test_email'])) {
+            $test_email = sanitize_email($_POST['test_email']);
+            $subject = sanitize_text_field($_POST['subject']);
+            $message = 'This is a test email sent from the MailPit SMTP Plugin for WordPress.';
+            $result = wp_mail($test_email, $subject, $message);
+
+            if ($result) {
+                echo '<div class="updated"><p>Test email sent successfully to ' . $test_email . '</p></div>';
+            } else {
+                echo '<div class="error"><p>Error sending test email to ' . $test_email . '</p></div>';
+            }
+        }
+        ?>
+    </div>
+    <?php
+}
+
+// Initialize and register SMTP settings
+function mailpit_smtp_settings_init() {
+    register_setting(
+        'mailpit-smtp-settings-group',
+        'mailpit_smtp_host'
+    );
+    register_setting(
+        'mailpit-smtp-settings-group',
+        'mailpit_smtp_port'
+    );
+
+    add_settings_section(
+        'mailpit-smtp-settings-section',
+        'SMTP Configuration',
+        'mailpit_smtp_settings_section_callback',
+        'mailpit-smtp-settings'
+    );
+
+    add_settings_field(
+        'mailpit-smtp-host',
+        'SMTP Host',
+        'mailpit_smtp_host_callback',
+        'mailpit-smtp-settings',
+        'mailpit-smtp-settings-section'
+    );
+
+    add_settings_field(
+        'mailpit-smtp-port',
+        'SMTP Port',
+        'mailpit_smtp_port_callback',
+        'mailpit-smtp-settings',
+        'mailpit-smtp-settings-section'
+    );
+}
+
+// Callback to display section description
+function mailpit_smtp_settings_section_callback() {
+    echo 'Configure your SMTP settings below:';
+}
+
+// Callback to display SMTP Host field
+function mailpit_smtp_host_callback() {
+    $host = esc_attr(get_option('mailpit_smtp_host'));
+    echo "<input type='text' name='mailpit_smtp_host' value='$host' />";
+}
+
+// Callback to display SMTP Port field
+function mailpit_smtp_port_callback() {
+    $port = esc_attr(get_option('mailpit_smtp_port'));
+    echo "<input type='text' name='mailpit_smtp_port' value='$port' />";
+}
+
+// Hook to add menu, initialize settings, and set default options
+add_action('admin_menu', 'mailpit_smtp_settings_menu');
+add_action('admin_init', 'mailpit_smtp_settings_init');
+add_action('admin_init', 'mailpit_smtp_default_options');
+
+// Override the WordPress mail system with SMTP settings
+function mailpit_smtp_phpmailer_init($phpmailer) {
+    $smtp_host = get_option('mailpit_smtp_host');
+    $smtp_port = get_option('mailpit_smtp_port');
+
+    $phpmailer->isSMTP();
+    $phpmailer->Host = $smtp_host;
+    $phpmailer->Port = $smtp_port;
+    $phpmailer->SMTPAuth = false;
+    // You can add more SMTP configuration options here, such as authentication settings if needed.
+}
+
+add_action('phpmailer_init', 'mailpit_smtp_phpmailer_init');
+
+// Define the uninstaller function
+function mailpit_smtp_uninstall() {
+    // Remove options from the database
+    delete_option('mailpit_smtp_host');
+    delete_option('mailpit_smtp_port');
+
+    // Optionally, you can perform additional cleanup tasks here
+    // For example, you can delete custom database tables, files, or any other plugin-specific data.
+
+    // Note: Be cautious when deleting data, and ensure it won't cause issues for users.
+
+    // You can also remove any custom roles or capabilities created by your plugin.
+}
+
+// Hook the uninstaller function to the 'uninstall' action
+register_uninstall_hook(__FILE__, 'mailpit_smtp_uninstall');
---
+++ readme.txt
@@ -0,0 +1,60 @@
+Plugin Name: MailPit SMTP for WordPress
+Description: A simple plugin to test SMTP settings and override the default WordPress mail system.
+Version: 1.0
+Author: Bishwajit Adhikary
+Author URI: https://bishwajitcadhikary.me
+Tags: smtp, wp mail smtp, wordpress smtp, mail, mailer, phpmailer, wp_mail, email, wp smtp
+Tested up to: WordPress 5.3.1 (Update this with the latest tested version)
+
+== Installation ==
+
+1. Upload the `mailpit-smtp` folder to the `/wp-content/plugins/` directory or install it through the WordPress plugin installer.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+
+== Configuration ==
+
+1. Navigate to 'MailPit SMTP' in your WordPress dashboard.
+2. Enter your SMTP host and port.
+4. Click the "Save Changes" button to save your SMTP settings.
+
+== Usage ==
+
+1. After configuring the SMTP settings, you can use the `wp_mail` function in your WordPress code to send emails via SMTP.
+2. To test your SMTP configuration, go to the 'MailPit SMTP Settings' page and use the "Send Test Email" feature.
+
+== Frequently Asked Questions (FAQ) ==
+
+= Q: How do I configure the SMTP settings? =
+A: Navigate to 'Settings' -> 'MailPit SMTP' and enter your SMTP host and port.
+
+= Q: Can I use authentication with SMTP? =
+A: Yes, you can configure SMTP authentication on the settings page.
+
+== Screenshots (optional) ==
+
+Nothing here yet.
+
+== Changelog ==
+
+1.0
+- Initial release.
+
+== Support ==
+
+If you encounter any issues or have questions, please visit the [support forum](https://wordpress.org/support/plugin/mailpit-smtp).
+
+== Contribute ==
+
+If you'd like to contribute to the development of this plugin, you can find it on [GitHub](https://github.com/bishwajitcadhikary/wp-mailpit-smtp).
+
+== License ==
+
+This plugin is released under the [GNU General Public License, Version 2](https://www.gnu.org/licenses/gpl-2.0.html).
+
+== Credits ==
+
+None yet.
+
+== Upgrade Notice (if needed) ==
+
+None yet.
